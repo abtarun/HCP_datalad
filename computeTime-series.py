b93cb2f4d751ea98698ecf5d3baf5a61d605b054
Anjali Tarun <47600352+abtarun@users.noreply.github.com>
2021-12-10 15:48:01 +0100

    Update computeTime-series.py

--- computeTime-series.py
+++ computeTime-series.py
@@ -1,10 +1,10 @@
-# This script computes the region-wise average of a nifti file. This script is particularly used in morphometric data.
+# This script computes the region-wise average of a nifti file. 
 
 # Input : atlas to use (e.g., Lausanne parcellation), and nifti volume
 
 # Output: vector (3D nifti) or data matrix (if nifti volume is 4D).
 
-# Usage : python computeTime-series.py $path-to-parcellation-nifti-files $path-to-functional-volume $subject-ID $directory-where-to-save-outputs $morphometric (e.g., 'area')
+# Usage : python computeTime-series.py $path-to-parcellation-nifti-files $path-to-functional-volume $subject-ID $directory-where-to-save-outputs
 # 
 # Author: Anjali Tarun
 
@@ -23,7 +23,7 @@ func_parc_dir=sys.argv[1]
 func=sys.argv[2]
 Id=sys.argv[3]
 save_dir=sys.argv[4]
-fil=sys.argv[5]
+
 
 for i in range(5):
     func_parc = os.path.join(func_parc_dir, 'sub-'+Id+'_atlas-L2018_res-scale'+str(i+1)+'_dseg.nii.gz')
@@ -35,8 +35,8 @@ for i in range(5):
     # Load the functional data (directly parcellated)
     time_series = masker.fit_transform(func)
 
-    p1 = os.path.join(save_dir, 'sub-'+Id+'_'+fil+'_atlas-L2018_res-scale'+str(i+1)+'_dseg.mat')
-    p2 = os.path.join(save_dir, 'sub-'+Id+'_'+fil+'_atlas-L2018_res-scale'+str(i+1)+'_dseg.npz')
+    p1 = os.path.join(save_dir, 'sub-'+Id+'_atlas-L2018_res-scale'+str(i+1)+'_dseg.mat')
+    p2 = os.path.join(save_dir, 'sub-'+Id+'_atlas-L2018_res-scale'+str(i+1)+'_dseg.npz')
 
     sio.savemat(p1,  {'average': time_series})
     savez(p2, average=time_series)
